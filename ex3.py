91c9970641e0b4f60b347b13e690cb3075879730
Alex <543024658@qq.com>
2018-08-02 23:46:50 +0800

    second

--- ex3.py
+++ ex3.py
@@ -1,23 +1,28 @@
+# 打印“I will now count my chickens”
 print("I will now count my chickens")
-
+# 打印“Hens 30.0”
 print("Hens", 25 + 30 / 6)
+# 打印“Roosters 97”
 print("Roosters", 100 - 25 * 3 % 4)
-
+# 打印“Now I will count the eggs:”
 print("Now I will count the eggs:")
-
+# 打印6.75
 print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
-
+# 打印“Is it true that 3 + 2 < 5 -7”
 print("Is it true that 3 + 2 < 5 -7")
-
+# 打印“False”
 print(3 + 2 < 5 - 7)
-
+# 打印“What is 3+2? 5”
 print("What is 3+2?", 3 + 2)
+# 打印“What is 5-7? -2”
 print("What is 5-7?", 5 - 7)
-
+# 打印“Oh, that's why it's False”
 print("Oh, that's why it's False")
-
+# 打印“How about some more.”
 print("How about some more.")
-
+# 打印“Is it qreater? True”
 print("Is it qreater?", 5 > -2)
+# 打印“Is it qreater or equal? True”
 print("Is it qreater or equal?", 5 >= -2)
-print("Is it less or equal?", 5 <= -2) 
+# 打印“Is it less or equal? False”
+print("Is it less or equal?", 5 <= -2)
