17c8e02a13474560a0d80ee00212d20d4902f52c
Alex <543024658@qq.com>
2021-11-29 15:42:50 +0800

    update

--- ex4.py
+++ ex4.py
@@ -1,10 +1,25 @@
+#汽车总数
 cars = 100
-space_in_a_car = 4
+
+#汽车定员
+space_in_a_car = 4.0
+
+#司机数量
 drivers = 30
+
+#乘客数量
 passengers = 90
+
+#没有司机的车
 cars_not_driven = cars - drivers
+
+#有司机的车
 cars_driven = drivers
+
+#
 carpool_capacity = cars_driven * space_in_a_car
+
+#平均每辆车乘客
 average_passengers_per = passengers / cars_driven
 
 print("There are", cars, "cars in avaiable.")
--- ex5.py
+++ ex5.py
@@ -10,7 +10,7 @@ print(f"Let's talk about {my_name}.")
 print(f"He's {my_height} inches tall.")
 print(f"He's {my_weight} pounds heavy.")
 print("Actually that's not too heavy.")
-print(f"He's got {my_eyes} eyes and {my_hair} .")
+print(f"He's got {my_eyes} eyes and {my_hair}.")
 print(f"His teeth are usually {my_teeth} depending on the coffee.")
 
 #this line is tricky try to get it exactly right
